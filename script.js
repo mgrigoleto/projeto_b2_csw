928e9fc08a932bbd258d5777f025b5f87fca68bc
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2022-05-13 16:51:29 -0300

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-function showFilhos(){
-  document.getElementById('qtd').style.display = 'block';
-  document.getElementById('menor').style.display = 'block';
-}
-function hideFilhos(){
-  document.getElementById('qtd').style.display = 'none';
-  document.getElementById('menor').style.display = 'none';
-}
