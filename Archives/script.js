06cdcbc5463f911709ce80357c1ec98f92a7e632
Maurício Grigoleto <102479476+mgrigoleto@users.noreply.github.com>
2022-05-13 16:51:12 -0300

    Add files via upload

---
+++ Archives/script.js
@@ -0,0 +1,8 @@
+function showFilhos(){
+  document.getElementById('qtd').style.display = 'block';
+  document.getElementById('menor').style.display = 'block';
+}
+function hideFilhos(){
+  document.getElementById('qtd').style.display = 'none';
+  document.getElementById('menor').style.display = 'none';
+}
